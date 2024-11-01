22a435509c3b7f8331abfde74605895616e07847
ArthurMitsuoYamamoto <arthuryamamoto0@gmail.com>
2024-10-31 22:22:34 -0300

    feat: finalizado

--- App.tsx
+++ App.tsx
@@ -1,17 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-
-import { ThemeProvider } from 'styled-components';
-
-import { Routes } from './src/routes';
-
-import theme from './src/global/styles/theme';
+import 'react-native-gesture-handler';
 import React from 'react';
+import { SafeAreaView, StatusBar, useColorScheme } from 'react-native';
+import { Provider } from 'react-redux';
+import { NavigationContainer } from '@react-navigation/native';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+import StackNavigator from './src/components/system/StackNavigator';
+import { store } from './src/store';
 
-export default function App() {
+function App(): JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    flex: 1,
+  };
+
   return (
-    <ThemeProvider theme={theme}>
-      <StatusBar backgroundColor="transparent" translucent />
-      <Routes />
-    </ThemeProvider>
+    <Provider store={store}>
+      <SafeAreaView style={backgroundStyle}>
+        <StatusBar
+          barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+          backgroundColor={backgroundStyle.backgroundColor}
+        />
+        <NavigationContainer>
+          <StackNavigator />
+        </NavigationContainer>
+      </SafeAreaView>
+    </Provider>
   );
 }
+
+export default App;
